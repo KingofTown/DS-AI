f1fd99644a3ac580b7c08a768ffc2f8e98a590c6
KingofTown <jocnnor@gmail.com>
2015-10-13 21:19:32 -0500

    Added onsave/onload fcns to cartographer

--- scripts/components/cartographer.lua
+++ scripts/components/cartographer.lua
@@ -4,6 +4,21 @@ local Cartographer = Class(function(self, inst)
 	self.utilityMap = {}
 end)
 
+function Cartographer:OnSave()
+	-- Build the save data map
+	local data = {}
+	-- Add the stuff to save
+	data.utilityMap = utilityMap
+	-- Return the map
+	return data
+end
+
+-- Load our utility map
+function Cartographer:OnLoad(data, newents)
+	if data and data.utilityMap then
+		self.utilityMap = data.utilityMap
+	end
+end
 
 -- This runs every game tick. Update the utility
 function Cartographer:OnUpdate(dt)
